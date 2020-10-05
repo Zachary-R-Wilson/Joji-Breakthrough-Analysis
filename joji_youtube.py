2c0212c46145ad45ca532d1d83c606345d8b6ecb
Zach <57845873+L0rd-Z-19@users.noreply.github.com>
2020-10-05 06:25:22 -0500

    added comments, and collected the data with the youtube api

--- joji_youtube.py
+++ joji_youtube.py
@@ -2,22 +2,32 @@ from youtube_api import YouTubeDataAPI
 import time
 import pandas as pd
 from client import api_key
+
+# Set up youtube api connection
 yt = YouTubeDataAPI(api_key)
 
+# Create a dictionary containing the ids for the playlists
 playlists = {'Nectar':'OLAK5uy_nq9PSQZCORK_TXwQuv6OL6pFIR_JuMfNM','Ballads 1':'OLAK5uy_mjAOCotH4qLPkVZdozo80M-H6jGLyhz7I','In Tongues':'OLAK5uy_mgqGb-owbElA7d0Mzx0xv2ZP52ykeVvFY'}
 
+# Titles for the playlists
+play_title = ['Nectar','Ballads 1','In Tongues']
+
+# Create a function to request information from the youtube api
 def joji(play_id):
+    # Dictionary to hold collected data
     metadata = {}
+    # Get each video in the playlist
     videos = yt.get_videos_from_playlist_id(play_id)
+    # For each video in the playlist collect the title, view count, like/dislike count, comment count, and the date it was published
     for vid in videos:
         meta = yt.get_video_metadata(video_id=vid['video_id'])
-        metadata[meta['video_title']] = {'title':meta['video_title'],'date':meta['video_publish_date'],'view count':meta['video_view_count'],'Like count':meta['video_like_count'], 'dislike count':meta['video_dislike_count'],'comments count':meta['video_comment_count']}
-        
+        metadata[meta['video_title']] = {'date':meta['video_publish_date'],'view count':meta['video_view_count'],'Like count':meta['video_like_count'], 'dislike count':meta['video_dislike_count'],'comments count':meta['video_comment_count']}
 
+    # Return the data we just collected    
     return(metadata)
 
-# print(joji(playlists['Nectar']))
-# time.sleep(2)
-# print(joji(playlists['Ballads 1']))
-# time.sleep(2)
-print(joji(playlists['In Tongues']))
+# For each playlist title, create a pandas dataframe and export the data as a csv
+for i in play_title:
+    df = pd.DataFrame.from_dict(joji(playlists[i]),orient='index')
+    df.to_csv(f'{i}.csv')
+    time.sleep(2)
