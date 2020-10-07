efedef8a7f9bf7255b657eb88a47f066ae13dfde
Zach <57845873+L0rd-Z-19@users.noreply.github.com>
2020-10-07 10:54:12 -0500

    Added a way to create a master file, and finished collecting the data

--- youtube.py
+++ youtube.py
@@ -1,19 +1,21 @@
 from youtube_api import YouTubeDataAPI
 import time
 import pandas as pd
-from client import api_key
+from keys import api_key
 
 # Set up youtube api connection
 yt = YouTubeDataAPI(api_key)
 
-# Create a dictionary containing the ids for the playlists
-playlists = {'Nectar':'OLAK5uy_nq9PSQZCORK_TXwQuv6OL6pFIR_JuMfNM','Ballads 1':'OLAK5uy_mjAOCotH4qLPkVZdozo80M-H6jGLyhz7I','In Tongues':'OLAK5uy_mgqGb-owbElA7d0Mzx0xv2ZP52ykeVvFY'}
-
 # Titles for the playlists
 play_title = ['Nectar','Ballads 1','In Tongues']
 
+
+# Create a dictionary containing the ids for the playlists
+# These can be found in the link
+playlists = {'Nectar':'OLAK5uy_nq9PSQZCORK_TXwQuv6OL6pFIR_JuMfNM','Ballads 1':'OLAK5uy_mjAOCotH4qLPkVZdozo80M-H6jGLyhz7I','In Tongues':'OLAK5uy_mgqGb-owbElA7d0Mzx0xv2ZP52ykeVvFY'}
+
 # Create a function to request information from the youtube api
-def joji(play_id):
+def youtube_pull(play_id):
     # Dictionary to hold collected data
     metadata = {}
     # Get each video in the playlist
@@ -28,6 +30,16 @@ def joji(play_id):
 
 # For each playlist title, create a pandas dataframe and export the data as a csv
 for i in play_title:
-    df = pd.DataFrame.from_dict(joji(playlists[i]),orient='index')
-    df.to_csv(f'{i}.csv')
+    df = pd.DataFrame.from_dict(youtube_pull(playlists[i]),orient='index')
+    df.to_csv(f'music_data/{i}.csv')
     time.sleep(2)
+
+# Read the .csv I just created
+nectar = pd.read_csv('music_data/nectar.csv')
+tongues = pd.read_csv('music_data/In Tongues.csv')
+ballads = pd.read_csv('music_data/Ballads 1.csv')
+
+# create a master .csv holding all the data together
+combined = nectar.append([tongues,ballads])
+combined.rename(columns={'Unnamed: 0': 'Title'}, inplace=True)
+combined.to_csv('music_data/joji.csv')
